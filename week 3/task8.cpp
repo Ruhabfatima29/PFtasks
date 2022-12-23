0917506fbbb9f53aaadbd42dde5afb6d4a9fe569
dell <ruhabfatima2003@gmail.com>
2022-12-23 08:46:06 +0500

    First commit

--- week 3/task8.cpp
+++ week 3/task8.cpp
@@ -9,11 +9,13 @@ double Bit;
 cout <<" Enter Bits : ";
 cin  >> Bit;
 Bytes= 8*Bit;
-cout << Bit << " is equal to " << Bytes << "Bytes";
+cout << Bit << " Bits is equal to " << Bytes << " Bytes";
+cout <<endl;
 KB = Bit*8000 ;
-cout << Bit  << " is equal to " << KB << "KB";
+cout << Bit  << " Bits is equal to " << KB   << " KB";
+cout << endl;
 MB = Bit * 8000000;
-cout << Bit  << " is equal to " << MB << "MB";
+cout << Bit  << " Bits is equal to " << MB   << " MB";
 
 
 
